af914705983a1581335660b33cbc5b6b152ebe9e
2chainzbear <jameswilcock8@gmail.com>
2020-09-26 01:58:41 -0700

    changed schedule method to excel

--- zoom.py
+++ zoom.py
@@ -3,30 +3,123 @@ import webbrowser
 import schedule
 import pyautogui
 import pathlib
+import openpyxl
 from secrets import *
+from converttime import convertpsttoutc
 parent = pathlib.Path(__file__).resolve().parent
 mypath = pathlib.Path(parent)
 
+
 # passcode is 678065
-# TODO make functionality so that maybe through a gui
-# or some other sort of config you can import classes and
-# generate the code
-# once I can make my schedule secret I will make public
+# TODO Check for errors in time has to be 09:53 rather than 9:53
+# convertpsttoutc should resolve these?
+# implement Start Date and End Date only join meetings during that interval
+# add functionality to connect a prerecorded video to the meeting
+
+def loadexcelfile():
+    excelpath = mypath / 'zoom.xlsx'
+    wb = openpyxl.load_workbook(excelpath)
+    sheet = wb['Sheet1']
+    numofcols = sheet.max_column
+    numofrows = sheet.max_row
+    zoomdata = []
+    loopdata = []
+    for x in range(2, numofrows + 1):
+        if sheet.cell(row=x, column=1).value is not None:
+            zoomdata.append([])
+        for y in range(1, numofcols + 1):
+            cellvalue = sheet.cell(row=x, column=y).value
+            if y == 1 and cellvalue is None:
+                break
+            else:
+                zoomdata[x - 2].append(cellvalue)
+            #print(sheet.cell(row=x, column=y).value)
+    return zoomdata
+
+
+def createschedule(zoomdata):
+    #[1] is link
+    #[2] is password, prob not needed many classes have code included
+    #[3] is time
+    #[4-10] monday - sunday
+    zoomlinks = []
+    zoompasses = []
+    zoomtimes = []
+    dayslist = []
+    for x in range(len(zoomdata)):
+        dayslist.append([])
+        zoomlinks.append(zoomdata[x][1])
+        if zoomdata[x][2] is not None:
+            zoompasses.append(zoomdata[x][2])
+        else:
+            zoompasses.append(-1)
+        zoomtimes.append(zoomdata[x][3])
+        for i in range(4, 11, 1):
+            if zoomdata[x][i] is not None:
+                dayslist[x].append(getday(i))
+    '''
+    print(dayslist)
+    print(zoomlinks)
+    print(zoompasses)
+    '''
+    for x in range(len(zoomdata)):
+        for i in range(len(dayslist[x])):
+            makeschedule(dayslist[x][i], zoomtimes[x],
+                         [zoomlinks[x], zoompasses[x]])
+    # iterate through list in range(len(zoomdata))
+    # for zoom links need to add a check to joinzoommeeting to see if it is link or code and password format
+
+
+def getday(daynum):
+    days = ["MONDAY", "TUESDAY", "WEDNESDAY",
+            "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+    return days[daynum - 4]
+
+
+def makeschedule(day, time, zoomdata):
+    print(f"Making Schedule {day}")
+    print(str(time))
+    if day.upper() == 'MONDAY':
+        schedule.every().monday.at(convertpsttoutc(str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'TUESDAY':
+        schedule.every().tuesday.at(convertpsttoutc(
+            str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'WEDNESDAY':
+        schedule.every().wednesday.at(convertpsttoutc(
+            str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'THURSDAY':
+        schedule.every().thursday.at(convertpsttoutc(
+            str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'FRIDAY':
+        schedule.every().friday.at(convertpsttoutc(str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'SATURDAY':
+        schedule.every().saturday.at(convertpsttoutc(
+            str(time))).do(joinzoommeeting, zoomdata)
+    elif day.upper() == 'SUNDAY':
+        schedule.every().sunday.at(convertpsttoutc(str(time))).do(joinzoommeeting, zoomdata)
 
 
 def joinzoommeeting(info):
     # info[0] classcode info [1] password if there is one
     # print(info[0],info[1])
     try:
-        webbrowser.open("https://oregonstate.zoom.us/j/" + str(info[0]))
-        loc = pyautogui.locateCenterOnScreen(str(mypath / 'passwordbox.png'))
-        while loc is None:
+        if info[1] != -1:
+            webbrowser.open("https://oregonstate.zoom.us/j/" + str(info[0]))
             loc = pyautogui.locateCenterOnScreen(
                 str(mypath / 'passwordbox.png'))
+            while loc is None:
+                loc = pyautogui.locateCenterOnScreen(
+                    str(mypath / 'passwordbox.png'))
+        else:
+            webbrowser.open(info[0])
         pyautogui.click(pyautogui.locateCenterOnScreen(
             str(mypath / 'closesymbol.png')))
-        pyautogui.click(loc)
-        pyautogui.write(info[1])
+        if info[1] != -1:
+            pyautogui.click(loc)
+            pyautogui.write(info[1])
+        time.sleep(3)
+        while pyautogui.locateCenterOnScreen(mypath / 'wating.png') is not None:
+            time.sleep(1)
         pyautogui.click(pyautogui.locateCenterOnScreen(
             str(mypath / 'joinmeeting.png')))
         time.sleep(5)
@@ -41,64 +134,11 @@ def joinzoommeeting(info):
     except IndexError:
         pyautogui.alert("Error data is not correctly entered")
         print("The code/password is not present")
-# print(convertpsttoutc("16:20"))
-# here I will make arrays first element is classcode second is the class password or -1 if there isnt
-# remove after 9/25
-
-
-def joinzoomwebinar():
-    webbrowser.open("https://oregonstate.zoom.us/w/99451242990?tk=DZbeEnoo9h6O8uzCbavHjUkrjxta2UyLyxDJZhDRAtE.DQIAAAAXJ8GJ7hZFMFpNV1dYMlJrU3NMOFlxdDh1eFdnAAAAAAAAAAAAAAAAAAAAAAAAAAAA&uuid=WN_rgFMBjX3T76Ue-rftu6lsw")
-    time.sleep(5)
-    pyautogui.click(pyautogui.locateCenterOnScreen(
-        str(mypath / 'closesymbol.png')))
-    time.sleep(2)
-    pyautogui.click(pyautogui.locateCenterOnScreen(
-        str(mypath / 'joinmeeting.png')))
-    time.sleep(5)
-    pyautogui.click(pyautogui.locateCenterOnScreen(
-        str(mypath / 'joincomaud.png')))
-    time.sleep(3)
-    pyautogui.click(pyautogui.locateCenterOnScreen(str(mypath / 'mute.png')))
-    time.sleep(2)
-    pyautogui.click(pyautogui.locateCenterOnScreen(
-        str(mypath / 'fullscreen.png')))
-
-
-schedule.every().monday.at("11:57").do(
-    joinzoommeeting, sus102)  # set 1 SUS102 MWF
-schedule.every().monday.at("14:57").do(
-    joinzoommeeting, mth264)  # set 2 MTH264 MWF
-schedule.every().tuesday.at("08:57").do(
-    joinzoommeeting, ph212)  # set 3 PH 212 Tues Thurs
-# schedule.every().tuesday.at("09:58").do(joinzoommeeting, eng104)  # set 4 Eng 104 Tues Thurs
-schedule.every().tuesday.at("13:57").do(
-    joinzoommeeting, ph212rec)  # PH 212 studio
-schedule.every().tuesday.at("15:57").do(
-    joinzoommeeting, hst104)  # set 5 HST 104 Tues Thurs
-schedule.every().tuesday.at("17:27").do(
-    joinzoommeeting, mth264rec)  # MTH264 rec
-schedule.every().wednesday.at("11:57").do(joinzoommeeting, sus102)  # set 1
-schedule.every().wednesday.at("14:57").do(joinzoommeeting, mth264)  # set 2
-schedule.every().wednesday.at("15:58").do(
-    joinzoommeeting, sus102lab)  # SUS 102 lab
-schedule.every().thursday.at("08:57").do(joinzoommeeting, ph212)  # set 3
-# schedule.every().thursday.at("09:58").do(joinzoommeeting, eng104)  # set 4
-# schedule.every().thursday.at("11:57").do(joinzoommeeting, [])  # PH212 lab ? so optional or meeting group
-schedule.every().thursday.at("15:57").do(joinzoommeeting, hst104)  # set 5
-schedule.every().friday.at("11:57").do(joinzoommeeting, sus102)  # set 1
-schedule.every().friday.at("14:57").do(joinzoommeeting, mth264)  # set 2
-schedule.every().friday.at("10:55").do(joinzoomwebinar)
-# schedule.every().second.do(joinzoommeeting,sus102)
-# 11:55am, 2:55pm monday
-# 9:00am, 9:57am ,1:55pm, 3:55pm, 5:25pm tues
-# 11:55am, 2:55pm, 3:55pm weds
-# 8:55am, 9:55am, 11:55am,3:55pm thurs
-# 11:55am, 2:55pm friday
-# webbrowser.open("https://zoom.us/")
-# joinzoommeeting(hst104)
 
 
 def main():
+    zoomdata = loadexcelfile()
+    createschedule(zoomdata)
     while True:
         time.sleep(1)
         schedule.run_pending()
