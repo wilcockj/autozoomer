d4dede8566ec2ae164fd8c8f9711b7366a5574f1
2chainzbear <jameswilcock8@gmail.com>
2020-10-10 15:21:07 -0700

    added bat file to make exe

---
+++ make.bat
@@ -0,0 +1,43 @@
+@echo off
+
+pyinstaller --onefile zoombot.py --noconsole --exclude-module matplotlib ^
+                           --exclude-module scipy ^
+                           --exclude-module setuptools ^
+                           --exclude-module hook ^
+                           --exclude-module distutils ^
+                           --exclude-module site ^
+                           --exclude-module hooks ^
+                           --exclude-module PyQt4 ^
+                           --exclude-module PyQt5 ^
+                           --exclude-module pydoc ^
+                           --exclude-module sqlite3 ^
+                           --exclude-module pyz ^
+                           --exclude-module pandas ^
+                           --exclude-module sklearn ^
+                           --exclude-module scapy ^
+                           --exclude-module scrapy ^
+                           --exclude-module sympy ^
+                           --exclude-module kivy ^
+                           --exclude-module pyramid ^
+                           --exclude-module opencv ^
+                           --exclude-module tensorflow ^
+                           --exclude-module pipenv ^
+                           --exclude-module pattern ^
+                           --exclude-module mechanize ^
+                           --exclude-module beautifulsoup4 ^
+                           --exclude-module wxPython ^
+                           --exclude-module pygi ^
+                           --exclude-module pillow ^
+                           --exclude-module pygame ^
+                           --exclude-module pyglet ^
+                           --exclude-module flask ^
+                           --exclude-module django ^
+                           --exclude-module pylint ^
+                           --exclude-module pytube ^
+                           --exclude-module odfpy ^
+                           --exclude-module mccabe ^
+                           --exclude-module pilkit ^
+                           --exclude-module wrapt ^
+                           --exclude-module astroid ^
+                           --exclude-module cv2 ^
+                           --exclude-module isort
